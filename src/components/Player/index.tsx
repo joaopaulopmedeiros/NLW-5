9cab906d9646fba50edcf9d67032f270090dbaaa
João Paulo <jpaulope@indracompany.com>
2021-04-20 10:44:32 -0300

    style: player container

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,8 +2,8 @@ import styles from './styles.module.scss';
 
 export default function Player() {
     return (
-        <>
+        <div className={styles.playerContainer}>
             <h1>Player</h1>
-        </>
+        </div>
     );
 }
