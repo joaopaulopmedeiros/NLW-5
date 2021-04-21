2050da37a1b963c5e889a593c643c0acffa81722
João Paulo <jpaulope@indracompany.com>
2021-04-21 20:20:55 -0300

    refactor: type for props of home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Olá, mundo! #NEXT</h1>
@@ -6,7 +18,7 @@ export default function Home() {
   )
 }
 
-export async function getStaticPropos() {
+export const getStaticPropos: GetStaticProps = async () => {
   const response = await fetch('http://localhost:3333/episodes');
   const data = await response.json();
 
