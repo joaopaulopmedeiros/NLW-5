d54feecb4f117d157299b5e1996c7367bd421e76
João Paulo <jpaulope@indracompany.com>
2021-04-21 20:38:04 -0300

    feat: async call to api with axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,41 @@
 import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
 type Episode = {
   id: string;
   title: string;
   members: string;
+  published_at: string;
 }
 
 type HomeProps = {
-  episodes: Episode[]
+  episodes: Episode[];
 }
 
 export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Olá, mundo! #NEXT</h1>
+      <p>
+        {JSON.stringify(props.episodes)}
+      </p>
     </>
   )
 }
 
-export const getStaticPropos: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
-
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+  
   return {
     props: {
       episodes: data,
-    }, 
+    },
     revalidate: 60 * 60 * 8,
   }
 }
